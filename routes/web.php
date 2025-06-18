cb25cc002908c3768ce8bf4be09a2688d2663270
yusmia09 <yusmiaws@gmail.com>
2025-06-19 00:51:45 +0700

    barubaru

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/', function () {
     return 'Laravel API is running 🎉';
